a6d9e19266cf7c807bbd40a0125264ff10e9027b
Art Dominick Vitor <artvitor@yahoo.com>
2016-10-17 08:15:36 +0800

    Add files via upload

---
+++ CollatzKemerut.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+main(){
+	ifstream m("input.txt");
+	int x;
+	m>>x;
+	while(x>1){
+			if((x%2)==1){
+				x=(x*3)+1;
+				cout<<x<<"\t";
+			}
+			if((x%2)==0){
+				x=x/2;
+				cout<<x<<"\t";
+			}
+		}
+	}
