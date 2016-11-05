fedcb81fa57d228e5b530eae894fd50e46896a9f
Art Dominick Vitor <artvitor@yahoo.com>
2016-11-05 08:53:24 +0800

    Update collatz.cpp

--- collatz.cpp
+++ collatz.cpp
@@ -1,19 +1,29 @@
 #include<iostream>
 #include<fstream>
+#include<cmath>
 using namespace std;
+
+int x, y, high=1, great=1, a, con, man;
 main(){
-	ifstream m("input.txt");
-	int x;
-	m>>x;
-	cout<<x<<"\t";
-	while(x>1){
-			if((x%2)==1){
-				x=(x*3)+1;
-				cout<<x<<"\t";
-			}
-			if((x%2)==0){
-				x=x/2;
-				cout<<x<<"\t";
+	ifstream m("H:\\My Documents\\input.txt");
+	while(m>>x>>y){
+	con=y;
+	for(a=x;a<y;a++){
+			man=a;
+			high=1;
+			while(man>1){
+				if ((man%2)==0) {
+					man=(man/2);
+					high++;
+				}
+				else if ((man%2)!=0) {
+					man=((3*man)+1);
+					high++;
+				}
+			if (high>great) great=high;
 			}
 		}
+		cout<<x<<" "<<y<<" "<<great<<endl;
 	}
+	return 0;	
+}
